39c6fa117280fc8c98c507c18ee3ea5faba62f56
Josemtobon <josem.tobon@udea.edu.co>
2025-04-27 20:58:36 -0500

    Change leader

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,36 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local action = wezterm.action
 
 -- Color Scheme
 config.color_scheme = "Tokyo Night"
 
+-- Font Settings
+config.font = wezterm.font("FiraCode Nerd Font")
+
 -- Tab Bar Appearance
 config.hide_tab_bar_if_only_one_tab = true
 
--- Window Padding
+local function is_nvim(pane)
+	local process_name = pane:get_foreground_process_name()
+	return process_name and process_name:find("n?vim") ~= nil
+end
+
+local function directional_keybinding(key, direction, nvim_command)
+	return {
+		key = key,
+		mods = "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_nvim(pane) then
+				win:perform_action(action.SendKey({ key = key, mods = "CTRL" }), pane)
+			else
+				win:perform_action(action.ActivatePaneDirection(direction), pane)
+			end
+		end),
+	}
+end
+
+-- Window Settings
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -16,22 +39,60 @@ config.window_padding = {
 }
 
 -- Keymaps
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	{
 		key = "|",
 		mods = "LEADER",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
+
 	{
 		key = "-",
 		mods = "LEADER",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
-	{ key = "h", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "CTRL", action = wezterm.action.ActivatePaneDirection("Right") },
+
+	{
+		key = "t",
+		mods = "LEADER",
+		action = action.SpawnTab("CurrentPaneDomain"),
+	},
+
+	directional_keybinding("h", "Left"),
+	directional_keybinding("j", "Down"),
+	directional_keybinding("k", "Up"),
+	directional_keybinding("l", "Right"),
+
+	{
+		key = "h",
+		mods = "CTRL|ALT",
+		action = action.AdjustPaneSize({ "Left", 1 }),
+	},
+
+	{
+		key = "j",
+		mods = "CTRL|ALT",
+		action = action.AdjustPaneSize({ "Down", 1 }),
+	},
+	{
+		key = "k",
+		mods = "CTRL|ALT",
+		action = action.AdjustPaneSize({ "Up", 1 }),
+	},
+	{
+		key = "l",
+		mods = "CTRL|ALT",
+		action = action.AdjustPaneSize({ "Right", 1 }),
+	},
 }
+
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = action.ActivateTab(i - 1),
+	})
+end
 
 return config
